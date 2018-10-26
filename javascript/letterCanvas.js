7c8e58ebb42216b0cc4e3ea631cd98ce49126093
Felicitas Deutges <deutges.ironhack@gmail.com>
2018-10-26 14:00:24 +0200

    last commit before presentation

--- javascript/letterCanvas.js
+++ javascript/letterCanvas.js
@@ -39,7 +39,7 @@ class LetterCanvas {
 
   typedCorrectWord() {
     scoreCounter++
-    if (scoreCounter == 5) {//change!!
+    if (scoreCounter == 2) {//change!!
       scoreCounter = 0;
       return false
     }
